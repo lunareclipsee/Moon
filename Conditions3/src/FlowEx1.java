5ab54484e4d5377d9a46256bd7d5abd1484af4af
TJ <TJ@DESKTOP-J2LIGQ3>
2020-02-12 15:45:38 +0900

    프로젝트 깃허브 연동

---
+++ Conditions3/src/FlowEx1.java
@@ -0,0 +1,22 @@
+
+public class FlowEx1 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+//		if문
+//		Expression(표현식)
+//		if(조건식 ) {
+//			조건식이 true일 때 수행될 문장들을 적는다
+//		}
+		
+		int visitCount = 0;
+		if(visitCount < 1) { //<--- 이 조건이 거짓이라면
+			//처음온 사람인지 확인하기 위해 1보다 작으면으로 표현
+			System.out.println("처음 오셨군요. 방문해주셔서 감사합니다.");
+				
+		} // 이뒤에 있는 것이 실행된다.
+		System.out.println(1234);
+	}
+
+}
